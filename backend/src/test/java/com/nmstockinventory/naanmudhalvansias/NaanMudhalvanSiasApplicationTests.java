42cc4bcc339043a57445efbe7ff6d6548fa13924
ASHIK SHARON M <80738185+ashiksharonm@users.noreply.github.com>
2023-11-25 18:38:25 +0530

    backend added

---
+++ backend/src/test/java/com/nmstockinventory/naanmudhalvansias/NaanMudhalvanSiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmstockinventory.naanmudhalvansias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaanMudhalvanSiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
