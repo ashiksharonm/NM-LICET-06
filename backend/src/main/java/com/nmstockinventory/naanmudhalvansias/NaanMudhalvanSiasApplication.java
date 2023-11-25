44023a56d48bd70cab55d582c29b5db4aeb1ee1b
ASHIK SHARON M <80738185+ashiksharonm@users.noreply.github.com>
2023-11-25 18:34:08 +0530

    backend added

---
+++ backend/src/main/java/com/nmstockinventory/naanmudhalvansias/NaanMudhalvanSiasApplication.java
@@ -0,0 +1,13 @@
+package com.nmstockinventory.naanmudhalvansias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaanMudhalvanSiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaanMudhalvanSiasApplication.class, args);
+	}
+
+}
